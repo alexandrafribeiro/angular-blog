333e96358ddf8e530171c6ac6179648d8b4ee9c0
alexandrafribeiro <fariasalexandraribeiro@gmail.com>
2022-12-10 21:40:23 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel anuncia um novo filme do Homem de Ferro, confira",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/823AF9C5228FB68380C33606C85E0EB8380B3169715FBF68AB12047124EE55E4/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"2",
+        "title": "Nova série anunciada no Disney+",
+        "description": "blablablabla",
+        "photoCover":"https://e-zoop.com/images/blog/cinema/sequencia-assistir-filmes-marvel-disney-filmes-e-series-1.jpg"
+    }
+]
